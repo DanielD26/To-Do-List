d145391dc9b096ece7b8f68e604655b6f75cebe9
DANIEL DIAZ <103547416@student.swin.edu.au>
2021-04-19 15:38:50 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function newItem() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("addItem").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  document.getElementById("list").appendChild(li);
+  document.getElementById("addItem").value = "";
+  
+
+    
+}
---
+++ create_vehicle_sql.sql
@@ -0,0 +1,8 @@
+DROP TABLE VEHICLE7416;
+
+CREATE TABLE VEHICLE7416 (
+    RegNo           VARCHAR (6) PRIMARY KEY
+,   Model           VARCHAR (10)
+,   Kiometers       INT
+,   PurchaseDate    DATE
+);
